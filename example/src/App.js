41cb7e25cc879d32b024ff74082706e7dc697002
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-05-15 13:56:24 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'tezid-proofs-component'
+import 'tezid-proofs-component/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
